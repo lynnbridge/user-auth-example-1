07660431a9b8c7bceb3b03124a1a3b20963aad0e
Kami Jean <kamijeanwilson@gmail.com>
2016-12-05 18:07:13 -0700

    Changing the port number

--- app.js
+++ app.js
@@ -57,6 +57,8 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3000, function () {
+var port_number = server.listen(process.env.PORT || 3000);
+
+app.listen(port_number, function () {
   console.log('Express app listening on port 3000');
 });
