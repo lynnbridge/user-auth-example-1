a0f355cbcfba834c847b9e7a91a327c17dbfe617
Kami Jean <kamijeanwilson@gmail.com>
2016-12-05 18:09:26 -0700

    Changing where the port code is located

--- app.js
+++ app.js
@@ -57,8 +57,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-var port_number = server.listen(process.env.PORT || 3000);
-
-app.listen(port_number, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Express app listening on port 3000');
 });
